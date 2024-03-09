85a7e99df7e50620ed69c8d96d623cc36d5e6912
ivanklopov <ivanklopov2000@yandex.ru>
2024-03-09 14:42:25 +0300

    rare update

--- static/JS/main.js
+++ static/JS/main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const cells = 31;
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -20,6 +19,7 @@ function loadItems() {
                 var li = document.createElement('li');
                 li.setAttribute('data-item', JSON.stringify(item.name));
                 li.classList.add('list__item');
+                li.classList.add('gradient_' + item.rare);
                 li.innerHTML = '<img src="' + item.img_url + '" alt="' + item.name + '" />' + '<p>' + item.name + '</p>';
                 list.appendChild(li);
             }
@@ -70,107 +70,9 @@ function start() {
 
                 var li = list.querySelectorAll('li')[i];
                 li.setAttribute('data-item', JSON.stringify(item.name));
-                li.innerHTML = '<img src="' + item.img_url + '" alt="' + item.name + '" />';
-            }
-            
-            function transitionEndHandler() {
-                var item = list.querySelectorAll('li')[winnerIndex];
-                item.classList.add('active');
-            }
-
-            // Применяем изменения к DOM перед запуском анимации
-            setTimeout(function() {
-                list.offsetHeight; // Принудительно обновляем макет для активации анимации
-                list.style.transition = ''; // Удаляем инлайн стиль, чтобы активировать анимацию
-                list.style.left = '50%';
-                list.style.transform = 'translate3d(-50%, 0, 0)';
-
-                list.addEventListener('transitionend', transitionEndHandler, { once: true });
-            }, 400);
-
-        } else {
-            console.error('HTTP error: ' + xhr.status);
-        }
-    };
-
-    xhr.onerror = function() {
-        console.error('Request failed');
-    };
-
-    xhr.send();
-=======
-const cells = 31;
-
-document.addEventListener('DOMContentLoaded', function() {
-    loadItems();
-});
-
-function loadItems() {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', '/get_items', true);
-
-    xhr.onload = function() {
-        if (xhr.status >= 200 && xhr.status < 400) {
-            var data = JSON.parse(xhr.responseText);
-            var list = document.querySelector('.list');
-            list.innerHTML = '';
-
-            for (var i = 0; i < 31; i++) {
-                var item = data.items[Math.floor(Math.random() * data.items.length)];
-                var li = document.createElement('li');
-                li.setAttribute('data-item', JSON.stringify(item.name));
-                li.classList.add('list__item');
+                li.classList.add('gradient_' + item.rare);
                 li.innerHTML = '<img src="' + item.img_url + '" alt="' + item.name + '" />' + '<p>' + item.name + '</p>';
-                list.appendChild(li);
-            }
-        } else {
-            console.error('Error:', xhr.status);
-        }
-    };
-
-    xhr.onerror = function() {
-        console.error('Request failed');
-    };
-
-    xhr.send();
-}
-
-function start() {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', '/choose_item', true);
-
-    xhr.onload = function() {
-        if (xhr.status >= 200 && xhr.status < 400) {
-            var data = JSON.parse(xhr.responseText);
-            var winnerIndex = 15; // Индекс победителя
-            var items = data.items;
-            
-            // Получаем список элементов
-            var list = document.querySelector('.list');
-            
-            var listItems = list.querySelectorAll('.list__item');
-            listItems.forEach(function(item) {
-                item.classList.remove('active');
-            });
-
-            // Устанавливаем начальные стили перед обновлением списка
-            list.style.transition = 'none';
-            list.style.left = '0';
-            list.style.transform = 'translate3d(0, 0, 0)';
-            
-            for (var i = 0; i < 31; i++) {
-                var item;
-                if (i === winnerIndex) {
-                    // Используем данные о победителе
-                    item = data.winner;
-                } else {
-                    // Используем данные об обычном предмете
-                    item = items[Math.floor(Math.random() * items.length)];
-                }
-
-                var li = list.querySelectorAll('li')[i];
-                li.setAttribute('data-item', JSON.stringify(item.name));
-                li.innerHTML = '<img src="' + item.img_url + '" alt="' + item.name + '" />';
+    
             }
             
             function transitionEndHandler() {
@@ -198,5 +100,4 @@ function start() {
     };
 
     xhr.send();
->>>>>>> refs/remotes/origin/main
 }
